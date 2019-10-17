06d1407a3584988326caca656936be1e9259173e
christoph <christophak@bmb.sdu.dk>
2019-10-17 10:08:45 +0000

    modified import counts dexseq

--- bin/dexseq_importCounts.R
+++ bin/dexseq_importCounts.R
@@ -1,7 +1,7 @@
 #!/usr/bin/Rscript --no-restore --no-environ --no-save
 
 # set libpaths to packrat local library
-source("../packrat/init.R")
+source("packrat/init.R")
 
 suppressPackageStartupMessages(library("readr"))
 suppressPackageStartupMessages(library("dplyr"))
@@ -13,14 +13,18 @@ suppressPackageStartupMessages(library("DEXSeq"))
 # email: christophak@bmb.sdu.dk
 #
 ################################################################################
+message("Collecting sample info...")
 sample_info <- read_csv(snakemake@input[["sample_info"]]) %>%
     filter(!is.na(illumina)) %>%
     mutate_at(vars(matches("condition")), as.factor) %>%
     mutate(path = file.path("dexseq", paste0(illumina, ".txt")))
 
+message("Creating DEXSeqDataSet...")
 DEXSeqDataSetFromHTSeq(
     countfiles = sample_info$path,
     sampleData = tibble::column_to_rownames(as.data.frame(sample_info), "illumina"),
     design = ~ condition_temp + exon + condition_temp:exon,
     flattenedfile = snakemake@input[["annotation"]]) %>%
     saveRDS(snakemake@output[[1]])
+
+message("Done")
