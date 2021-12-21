7c2cf3cf2a6a835f976ef6583a55384d7d63f7c6
christoph <christophak@bmb.sdu.dk>
2021-12-22 00:54:33 +0100

    redone browser tracks

---
+++ bin/browserTracks.R
@@ -0,0 +1,245 @@
+
+source(".Rprofile")
+suppressPackageStartupMessages({
+    library("GenomicFeatures")
+    library("Gviz")
+    library("dplyr")
+    library("readr")
+    library("AnnotationDbi")
+})
+
+
+txdb_gencode <- loadDb(snakemake@input[["txdb"]])
+txdb_gencode_dump <- as.list(txdb_gencode)
+
+txdb_flair <- loadDb(snakemake@input[["txdb_flair"]])
+txdb_flair_dump <- as.list(txdb_flair)
+
+txdb_stringtie <- loadDb(snakemake@input[["txdb_stringtie"]])
+txdb_stringtie_dump <- as.list(txdb_stringtie)
+
+stringtie_refGene <- read_tsv(snakemake@input$tmap,
+    col_types = "cccccidddici",
+    na = "-") %>%
+    dplyr::select(ref_gene_id, qry_gene_id) %>%
+    tidyr::drop_na() %>%
+    distinct()
+
+faidx <- read_tsv(snakemake@input$genome,
+        col_types = "cidii",
+        col_names = c("chrom", "length", "offset", "LINEBASES", "LINEWIDTH")) %>%
+    dplyr::select(chrom, length) %>%
+    mutate(is_circular = FALSE) %>%
+    as.data.frame()
+
+
+
+
+subset.txdb <- function(txdb, txdb_dump, gene_id, chrominfo = faidx) {
+
+    all_tx <- suppressMessages(AnnotationDbi::select(txdb, gene_id,
+            c("GENEID", "TXNAME"), "GENEID")) %>%
+        pull(TXNAME)
+
+    all_tx_ids <- txdb_dump[[1]] %>%
+        as_tibble() %>%
+        filter(tx_name %in% all_tx) %>%
+        pull(tx_id)
+
+    new_txdb_dump <- vector("list", 4)
+    new_txdb_dump[1:3] <- txdb_dump[1:3] %>%
+        purrr::map(filter, tx_id %in% all_tx_ids)
+    new_txdb_dump$chrominfo <- chrominfo
+
+    do.call(makeTxDb, new_txdb_dump)
+}
+
+
+plot.transcripts <- function(gene_id, max_cov_h3k4 = 5, max_cov_cdna = 300,
+    max_cov_illumina = 2000, extend = 2500, lwd_sashimi_max = 3, temp = "warm") {
+
+    message(sprintf("Preparing %s...", gene_id))
+
+    gene_info <- suppressMessages(AnnotationDbi::select(txdb_gencode, keys = gene_id,
+            keytype = "GENEID",
+            columns = c("TXCHROM", "TXSTART", "TXEND"))) %>%
+        as_tibble() %>%
+        group_by(GENEID) %>%
+        summarise(TXCHROM = unique(TXCHROM),
+            TXSTART = min(TXSTART),
+            TXEND = max(TXEND))
+
+
+    # gencode
+    txdb_gencode_subset <- subset.txdb(txdb_gencode, txdb_gencode_dump, gene_id)
+
+    gencodetrack <- GeneRegionTrack(txdb_gencode_subset,
+        chromosome = gene_info$TXCHROM,
+        min.height = 3,
+        from = gene_info$TXSTART, to = gene_info$TXEND,
+        name = "M22")
+
+
+    # flair
+    txdb_flair_subset <- subset.txdb(txdb_flair, txdb_flair_dump, gene_id)
+
+    flairtrack <- GeneRegionTrack(txdb_flair_subset,
+        chromosome = gene_info$TXCHROM,
+        min.height = 3,
+        from = gene_info$TXSTART, to = gene_info$TXEND,
+        name = "flair cDNA")
+
+
+    # stringtie
+    txdb_stringtie_subset <- subset.txdb(txdb_stringtie, txdb_stringtie_dump,
+        stringtie_refGene %>%  filter(ref_gene_id == gene_id) %>%  pull(qry_gene_id))
+
+    stringtietrack <- GeneRegionTrack(txdb_stringtie_subset,
+        chromosome = gene_info$TXCHROM,
+        min.height = 3,
+        from = gene_info$TXSTART, to = gene_info$TXEND,
+        name = "strtie telo")
+
+
+    # list of unique exons for sashimi plot
+    unique_exons <- c(txdb_flair_subset, txdb_stringtie_subset, txdb_gencode_subset) %>%
+        purrr::map(intronsByTranscript) %>% # extra intron information
+        purrr::reduce(c) %>% # collapse the lists into one single list
+        unlist() %>% # concatenate all exons of all transcripts into one granges object
+        unique() # remove duplicate exons
+
+
+    # scale
+    gtrack <- GenomeAxisTrack(from = min(gene_info$TXSTART),
+        to = max(gene_info$TXEND),
+        scale = 0.25)
+
+
+    # bam tracks
+    if(temp == "warm") {
+
+        cdna <- AlignmentsTrack(snakemake@input[["cdna_warm"]],
+            isPaired = FALSE,
+            genome = "mm10",
+            name = "cDNA 22°C",
+            ylim = c(0, max_cov_cdna),
+            type = c("pileup"),
+            min.height = 3, # minimum height of a read in px, controls how many read are plotted
+            chromosome = gene_info$TXCHROM,
+            start = gene_info$TXSTART,
+            end = gene_info$TXEND)
+
+        illumina <- AlignmentsTrack(snakemake@input[["illumina_warm"]],
+            isPaired = TRUE,
+            genome = "mm10",
+            name = "Illu 22°C",
+            ylim = c(0, max_cov_illumina),
+            type = c("coverage", "sashimi"),
+            lwd.sashimiMax = lwd_sashimi_max,
+            sashimiHeight = .33,
+            transformation = function(x) log1p(x),
+            chromosome = gene_info$TXCHROM,
+            start = gene_info$TXSTART,
+            end = gene_info$TXEND)
+
+    } else if (temp == "cold") {
+
+        cdna <- AlignmentsTrack(snakemake@input[["cdna_cold"]],
+            isPaired = FALSE,
+            genome = "mm10",
+            name = "cDNA 4°C",
+            ylim = c(0, max_cov_cdna),
+            type = c("pileup"),
+            min.height = 3, # minimum height of a read in px, controls how many read are plotted
+            chromosome = gene_info$TXCHROM,
+            start = gene_info$TXSTART,
+            end = gene_info$TXEND)
+
+        illumina <- AlignmentsTrack(snakemake@input[["illumina_cold"]],
+            isPaired = TRUE,
+            genome = "mm10",
+            name = "Illu 4°C",
+            ylim = c(0, max_cov_illumina),
+            type = c("coverage", "sashimi"),
+            lwd.sashimiMax = lwd_sashimi_max,
+            sashimiHeight = .33,
+            transformation = function(x) log1p(x),
+            chromosome = gene_info$TXCHROM,
+            start = gene_info$TXSTART,
+            end = gene_info$TXEND)
+    }
+
+    # # ChIP seq
+    # ncd_warm_h3k4me3 <- DataTrack(snakemake@input[["ncd_warm_h3k4me3"]],
+    #     genome = "mm10",
+    #     name = "H3K4me3 22 NCD",
+    #     ylim = c(0, max_cov_h3k4),
+    #     chromosome = unique(gene_info$TXCHROM),
+    #     start = min(gene_info$TXSTART) - extend,
+    #     end = max(gene_info$TXEND) + extend,
+    #     type = "histogram")
+    #
+    # ncd_cold_h3k4me3 <- DataTrack(snakemake@input[["ncd_cold_h3k4me3"]],
+    #     genome = "mm10",
+    #     name = "H3K4me3 4 NCD",
+    #     ylim = c(0, max_cov_h3k4),
+    #     chromosome = unique(gene_info$TXCHROM),
+    #     start = min(gene_info$TXSTART) - extend,
+    #     end = max(gene_info$TXEND) + extend,
+    #     type = "histogram")
+
+
+    # Plotting
+    options(ucscChromosomeNames = FALSE)
+
+    tracks <- list(gtrack,
+            illumina,
+            cdna,
+#           ncd_warm_h3k4me3, ncd_cold_h3k4me3,
+            gencodetrack, flairtrack, stringtietrack)
+
+
+    if(length(extend) == 1) {
+
+        extend_l <- extend
+        extend_r <- extend
+
+    } else if (length(extend) == 2) {
+
+        extend_l <- extend[1]
+        extend_r <- extend[2]
+
+    }
+
+
+    plotTracks(
+        tracks,
+        from = min(gene_info$TXSTART) - extend_l,
+        to = max(gene_info$TXEND) + extend_r,
+        extend.left = 250,
+        extend.right = 250,
+        background.title = "darkblue",
+        cex = .75,
+        cex.axis = .6,
+        sashimiFilter = unique_exons,
+        lwd = .25,
+        sizes = c(.1,
+              .33,               #illumina
+              1,                 #ont
+#              rep(.5, 2),       #chip
+              rep(.25, 3)))      #annotation
+}
+
+pdf(snakemake@output[[1]],
+    width = snakemake@params$dimsenion[1],
+    height = snakemake@params$dimension[2])
+
+    plot.transcripts(
+        gene_id = snakemake@params$gene_id,
+        max_cov_cdna = snakemake@params$max_cov_cdna,
+        max_cov_illumina = snakemake@params$max_cov_illumina,
+        lwd_sashimi_max = snakemake@params$lwd_sashimi_max,
+        extend = snakemake@params$extend_plot,
+        temp = snakemake@params$temp)
+
+dev.off()
--- bin/browserTracks.smk
+++ bin/browserTracks.smk
@@ -1,8 +1,9 @@
-rule browserTracksAdcy3Cars2:
+rule browserTrackAdcy3:
     input:
         txdb = "data/annotation/annotation_txdb.sqlite",
         txdb_flair = "data/reannotation/flair/annotation/cdna_flair.isoforms_txdb.sqlite",
         txdb_stringtie = "data/reannotation/stringtie/cdna_stringtie_noUnknownStrand_txdb.sqlite",
+        tmap = "data/reannotation/stringtie/gffcmp.cdna_stringtie.gtf.tmap",
         illumina_warm = "data/bam/illumina/5034_S33_Aligned.sortedByCoord.out.bam",
         illumina_cold = "data/bam/illumina/5035_S34_Aligned.sortedByCoord.out.bam",
         cdna_warm = "data/bam/cdna/merged/cdna_barcode07_genome.bam",
@@ -10,7 +11,69 @@ rule browserTracksAdcy3Cars2:
         ncd_warm_h3k4me3 = "data/additionalBrowserTracks/200901_rute_chip_adipocytes/2_warm_ncd_H3K4me3.bw",
         ncd_cold_h3k4me3 =  "data/additionalBrowserTracks/200901_rute_chip_adipocytes/7_warm_hfd_H3K4me3.bw",
         genome = "data/annotation/genome.fa.fai"
+    params:
+        gene_id = "ENSMUSG00000020654.15",
+        max_cov_cdna = 450,
+        max_cov_illumina = 5,
+        lwd_sashimi_max = 10,
+        extend_plot = 0,
+        temp = "warm",
+        dimension = [6.85, 4.4525]
     output:
-        "res/browserTracks/nanopore_iBAT_browserTracks_Adcy3Cars2.html"
+        "res/browserTracks/Adcy3.pdf"
     script:
-        "browserTracks_Adcy3Cars2.Rmd"
+        "browserTracks.R"
+
+
+rule browserTrackCars2:
+    input:
+        txdb = "data/annotation/annotation_txdb.sqlite",
+        txdb_flair = "data/reannotation/flair/annotation/cdna_flair.isoforms_txdb.sqlite",
+        txdb_stringtie = "data/reannotation/stringtie/cdna_stringtie_noUnknownStrand_txdb.sqlite",
+        tmap = "data/reannotation/stringtie/gffcmp.cdna_stringtie.gtf.tmap",
+        illumina_warm = "data/bam/illumina/5034_S33_Aligned.sortedByCoord.out.bam",
+        illumina_cold = "data/bam/illumina/5035_S34_Aligned.sortedByCoord.out.bam",
+        cdna_warm = "data/bam/cdna/merged/cdna_barcode07_genome.bam",
+        cdna_cold = "data/bam/cdna/merged/cdna_barcode08_genome.bam",
+        ncd_warm_h3k4me3 = "data/additionalBrowserTracks/200901_rute_chip_adipocytes/2_warm_ncd_H3K4me3.bw",
+        ncd_cold_h3k4me3 =  "data/additionalBrowserTracks/200901_rute_chip_adipocytes/7_warm_hfd_H3K4me3.bw",
+        genome = "data/annotation/genome.fa.fai"
+    params:
+        gene_id = "ENSMUSG00000056228.10",
+        max_cov_cdna = 250,
+        max_cov_illumina = 6,
+        lwd_sashimi_max = 10,
+        temp = "cold",
+        extend_plot = 400,
+        dimension = [6.85, 4.4525]
+    output:
+        "res/browserTracks/Cars2.pdf"
+    script:
+        "browserTracks.R"
+
+
+rule browserTrackPde4d:
+    input:
+        txdb = "data/annotation/annotation_txdb.sqlite",
+        txdb_flair = "data/reannotation/flair/annotation/cdna_flair.isoforms_txdb.sqlite",
+        txdb_stringtie = "data/reannotation/stringtie/cdna_stringtie_noUnknownStrand_txdb.sqlite",
+        tmap = "data/reannotation/stringtie/gffcmp.cdna_stringtie.gtf.tmap",
+        illumina_warm = "data/bam/illumina/5034_S33_Aligned.sortedByCoord.out.bam",
+        illumina_cold = "data/bam/illumina/5035_S34_Aligned.sortedByCoord.out.bam",
+        cdna_warm = "data/bam/cdna/merged/cdna_barcode07_genome.bam",
+        cdna_cold = "data/bam/cdna/merged/cdna_barcode08_genome.bam",
+        ncd_warm_h3k4me3 = "data/additionalBrowserTracks/200901_rute_chip_adipocytes/2_warm_ncd_H3K4me3.bw",
+        ncd_cold_h3k4me3 =  "data/additionalBrowserTracks/200901_rute_chip_adipocytes/7_warm_hfd_H3K4me3.bw",
+        genome = "data/annotation/genome.fa.fai"
+    params:
+        gene_id = "ENSMUSG00000021699.17",
+        max_cov_cdna = 250,
+        max_cov_illumina = 6,
+        lwd_sashimi_max = 10,
+        temp = "warm",
+        extend_plot = [-1.175E6, 3000],
+        dimension = [6.85, 6.85]
+    output:
+        "res/browserTracks/Pde4d.pdf"
+    script:
+        "browserTracks.R"
